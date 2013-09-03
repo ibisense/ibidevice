1ef8d4d24f4fced67ec3da0875c00682ffd2f3e9
Mikko Syrjälahti <mikko@ibisense.com>
2013-09-03 10:47:46 +0300

    Do not output errors if no 1 wire devices are available

--- collectors/01_1wire_temp.sh
+++ collectors/01_1wire_temp.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 for dir in /sys/bus/w1/drivers/w1_slave_driver/28*
 do
- CH=`echo "$dir" | sed -e "s/.*-//"`
+ if  ! [[  "$dir"  =~ \*$ ]] ; then 
+CH=`echo "$dir" | sed -e "s/.*-//"`
  DATA=`cat $dir/w1_slave`
  if [ `echo $DATA|awk '{print $12;}'` = "YES" ] ; then
    echo $DATA| awk "{print \"$CH \" substr(\$22,3,5)/1000; }"
  fi
+fi
 done
+
 
